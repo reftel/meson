990af253e5ebdf9112f1d3d240398182d5def769
Jussi Pakkanen <jpakkane@gmail.com>
2024-05-26 23:50:49 +0300

    Hook up new option store to the old code. Can compile simple projects.

--- mesonbuild/options.py
+++ mesonbuild/options.py
@@ -694,6 +694,7 @@ class OptionStore:
         return basic + build
 
     def add_system_option(self, name, value_object):
+        assert isinstance(name, str)
         cname = self.form_canonical_keystring(name)
         # FIXME; transfer the old value for combos etc.
         if cname not in self.options:
